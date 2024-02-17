a22138b9c419d2847dfd0f216a2f38f601b08e41
Adit0507 <adi05072002@gmail.com>
2024-02-17 15:18:11 +0530

    working....

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Healthchecker",
+		Usage: "Checks whether a website is running or down",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "domain",
+				Aliases:  []string{"d"},
+				Usage:    "Domain Name to check",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "port",
+				Aliases:  []string{"p"},
+				Usage:    "Port number to check",
+				Required: false,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			port := c.String("port")
+			if c.String("port") == "" {
+				port = "80"
+			}
+			status := Check(c.String("domain"), port)
+			fmt.Println(status)
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
